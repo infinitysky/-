f355635cade47cbee4a96caca37364c7673d8305
Cliff Li <dreamli1121@gmail.com>
2017-04-24 17:16:16 +1000

    UI error fix

--- onmyouji/src/app/app.component.ts
+++ onmyouji/src/app/app.component.ts
@@ -1,10 +1,136 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app works!';
+
+  public value_num_2x_01: any;
+  public value_num_2x_02: any;
+  public value_num_2x_03: any;
+  public value_num_2x_04: any;
+  public value_num_2x_05: any;
+  public value_num_2x_06: any;
+  public value_num_2x_07: any;
+  public value_num_2x_08: any;
+  public value_num_2x_09: any;
+  public value_num_2x_10: any;
+  public value_num_2x_11: any;
+  public value_num_2x_12: any;
+  public value_num_2x_13: any;
+  public value_num_2x_14: any;
+  public value_num_2x_15: any;
+  public value_num_2x_16: any;
+  public value_num_2x_17: any;
+  public value_num_2x_18: any;
+  public value_num_2x_19: any;
+  public value_num_2x_20: any;
+  public value_num_2x_0209: any;
+  public value_num_2x_1015: any;
+  public value_num_2x_1617: any;
+  public value_num_2x_1819: any;
+
+
+  public value_num_3x_20: any;
+  public value_num_3x_21: any;
+  public value_num_3x_22: any;
+  public value_num_3x_23: any;
+  public value_num_3x_24: any;
+  public value_num_3x_25: any;
+  public value_num_4x_25: any;
+  public value_num_4x_26: any;
+  public value_num_4x_27: any;
+  public value_num_4x_28: any;
+  public value_num_4x_29: any;
+  public value_num_4x_30: any;
+  public value_num_5x_all: any;
+
+
+
+
+  ngOnInit() {
+
+  }
+
+  constructor() {
+
+  }
+
+
+  calculation(){
+
+    let num_2x_01 = parseFloat( this.value_num_2x_01 );	let exp_2x_01 = 0;	let exp201 = num_2x_01 * exp_2x_01;
+    let num_2x_02 = parseFloat( this.value_num_2x_02 );	let exp_2x_02 = 180;	let exp202 = num_2x_02 * exp_2x_02;
+    let num_2x_03 = parseFloat( this.value_num_2x_03 );	let exp_2x_03 = 420;	let exp203 = num_2x_03 * exp_2x_03;
+    let num_2x_04 = parseFloat( this.value_num_2x_04 );	let exp_2x_04 = 840;	let exp204 = num_2x_04 * exp_2x_04;
+    let num_2x_05 = parseFloat( this.value_num_2x_05 );	let exp_2x_05 = 1470;	let exp205 = num_2x_05 * exp_2x_05;
+    let num_2x_06 = parseFloat( this.value_num_2x_06 );	let exp_2x_06 = 2510;	let exp206 = num_2x_06 * exp_2x_06;
+    let num_2x_07 = parseFloat( this.value_num_2x_07 );	let exp_2x_07 = 3950;	let exp207 = num_2x_07 * exp_2x_07;
+    let num_2x_08 = parseFloat( this.value_num_2x_08);	let exp_2x_08 = 6110;	let exp208 = num_2x_08 * exp_2x_08;
+    let num_2x_09 = parseFloat( this.value_num_2x_09 );	let exp_2x_09 = 8900;	let exp209 = num_2x_09 * exp_2x_09;
+    let num_2x_10 = parseFloat( this.value_num_2x_10 );	let exp_2x_10 = 12800;	let exp210 = num_2x_10 * exp_2x_10;
+    let num_2x_11 = parseFloat( this.value_num_2x_11);	let exp_2x_11 = 17600;	let exp211 = num_2x_11 * exp_2x_11;
+    let num_2x_12 = parseFloat( this.value_num_2x_12 );	let exp_2x_12 = 23980;	let exp212 = num_2x_12 * exp_2x_12;
+    let num_2x_13 = parseFloat( this.value_num_2x_13 );	let exp_2x_13 = 31570;	let exp213 = num_2x_13 * exp_2x_13;
+    let num_2x_14 = parseFloat( this.value_num_2x_14 );	let exp_2x_14 = 41290;	let exp214 = num_2x_14 * exp_2x_14;
+    let num_2x_15 = parseFloat( this.value_num_2x_15 );	let exp_2x_15 = 52570;	let exp215 = num_2x_15 * exp_2x_15;
+    let num_2x_16 = parseFloat( this.value_num_2x_16 );	let exp_2x_16 = 66610;	let exp216 = num_2x_16 * exp_2x_16;
+    let num_2x_17 = parseFloat( this.value_num_2x_17 );	let exp_2x_17 = 82600;	let exp217 = num_2x_17 * exp_2x_17;
+    let num_2x_18 = parseFloat( this.value_num_2x_18 );	let exp_2x_18 = 102060;	let exp218 = num_2x_18 * exp_2x_18;
+    let num_2x_19 = parseFloat( this.value_num_2x_19 );	let exp_2x_19 = 123900;	let exp219 = num_2x_19 * exp_2x_19;
+    let num_2x_20 = parseFloat( this.value_num_2x_20 );	let exp_2x_20 = 150000;	let exp220 = num_2x_20 * exp_2x_20;
+
+    let num_2x_0209 = parseFloat( this.value_num_2x_0209 );	let exp_2x_0209 = 5000;	let exp20209 = num_2x_0209 * exp_2x_0209;
+    let num_2x_1015 = parseFloat( this.value_num_2x_1015 );	let exp_2x_1015 = 30000;	let exp21015 = num_2x_1015 * exp_2x_1015;
+    let num_2x_1617 = parseFloat( this.value_num_2x_1617  );	let exp_2x_1617 = 80000;	let exp21617 = num_2x_1617 * exp_2x_1617;
+    let num_2x_1819 = parseFloat( this.value_num_2x_1819 );	let exp_2x_1819 = 120000;	let exp21819 = num_2x_1819 * exp_2x_1819;
+
+    let num_3x_20 = parseFloat( this.value_num_3x_20 );	let exp_3x_20 = 150000;	let exp320 = num_3x_20 * exp_3x_20;
+    let num_3x_21 = parseFloat( this.value_num_3x_21 );	let exp_3x_21 = 178950;	let exp321 = num_3x_21 * exp_3x_21;
+    let num_3x_22 = parseFloat( this.value_num_3x_22 );	let exp_3x_22 = 213030;	let exp322 = num_3x_22 * exp_3x_22;
+    let num_3x_23 = parseFloat( this.value_num_3x_23 );	let exp_3x_23 = 250470;	let exp323 = num_3x_23 * exp_3x_23;
+    let num_3x_24 = parseFloat( this.value_num_3x_24 );	let exp_3x_24 = 293990;	let exp324 = num_3x_24 * exp_3x_24;
+    let num_3x_25 = parseFloat( this.value_num_3x_25 );	let exp_3x_25 = 341420;	let exp325 = num_3x_25 * exp_3x_25;
+    let num_4x_25 = parseFloat( this.value_num_4x_25 );	let exp_4x_25 = 791420;	let exp425 = num_4x_25 * exp_4x_25;
+    let num_4x_26 = parseFloat( this.value_num_4x_26 );	let exp_4x_26 = 845960;	let exp426 = num_4x_26 * exp_4x_26;
+    let num_4x_27 = parseFloat( this.value_num_4x_27 );	let exp_4x_27 = 905000;	let exp427 = num_4x_27 * exp_4x_27;
+    let num_4x_28 = parseFloat( this.value_num_4x_28 );	let exp_4x_28 = 972260;	let exp428 = num_4x_28 * exp_4x_28;
+    let num_4x_29 = parseFloat( this.value_num_4x_29 );	let exp_4x_29 = 1044650;	let exp429 = num_4x_29 * exp_4x_29;
+    let num_4x_30 = parseFloat( this.value_num_4x_30 );	let exp_4x_30 = 1126450;	let exp430 = num_4x_30 * exp_4x_30;
+    let num_5x_all = parseFloat( this.value_num_5x_all );	let exp_5x_all = 4292130;	let exp5all = num_5x_all * exp_5x_all;
+
+    let num2x = (num_2x_01 + num_2x_0209 + num_2x_1015 + num_2x_1617 + num_2x_1819 + num_2x_20);
+    let num3x = (num_3x_20 + num_3x_21 + num_3x_22 + num_3x_23 + num_3x_24 + num_3x_25);
+    let num4x = (num_4x_25 + num_4x_26 + num_4x_27 + num_4x_28 + num_4x_29 + num_4x_30);
+    let num5x = (num_5x_all);
+
+    let exp2x = (exp201 + exp20209 + exp21015 + exp21617 + exp21819 + exp220);
+    let exp3x = (exp320 + exp321 + exp322 + exp323 + exp324 + exp325);
+    let exp4x = (exp425 + exp426 + exp427 + exp428 + exp429 + exp430);
+    let exp5x = (exp5all);
+
+    let datacard = (300-(num5x*60)-(num4x*12)-(num3x*3)-num2x);
+    if (datacard<0) {
+      datacard = 0;
+    }
+    let expfull = ( exp5x + exp4x + exp3x + exp2x);
+    if (expfull > 21460650) { expfull = 21460650;}
+
+    let expall = 21460650;
+
+
+    console.log(expfull);
+
+
+
+
+
+
+  }
+
+
 }
